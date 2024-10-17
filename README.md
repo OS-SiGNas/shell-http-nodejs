7ad114256ceeec14f36b94163450462e62dc978f
OS-SiGNas <signas13@gmail.com>
2024-10-16 23:22:33 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# HTTP SHELL!
+
+This is a simple Http Shell written in typescript, which does not depend on external libraries such as express, fastify or koa; is a simple script that uses nodejs native http; it consists of only 4 GET endpoints, **/sh** to run commands, and **/file** to view files.
+
+## /sh
+
+this endpoint receive command query in the url, like **/sh?command=ls** or **/sh?command=cat%20file.txt**
+
+## /file
+
+this endpoint receive file path like **/file?name=/home/user/image.png**
+
+## /info
+
+this endpoint return system info
+
+## /status
+
+this endpoint return 204 status code
---
+++ src/http-shell.ts
@@ -0,0 +1,97 @@
+import { readFile } from "node:fs/promises";
+import { exec } from "node:child_process";
+import { env, argv, exit } from "node:process";
+import { promisify } from "node:util";
+import { extname } from "node:path";
+import { IncomingMessage, Server, ServerResponse } from "node:http";
+
+type Controller = (req: IncomingMessage, res: ServerResponse<IncomingMessage> & { req: IncomingMessage }) => void;
+
+const ShellHttp = class {
+  constructor() {
+    const server = new Server();
+    server.on("request", (request, response) => {
+      this.#logger(request, response);
+      if (request.method !== "GET") return response.writeHead(404).end();
+      if (request.url?.includes("/sh")) return this.#shHandler(request, response);
+      if (request.url?.includes("/file")) return this.#fileHandler(request, response);
+      else return response.writeHead(404).end();
+    });
+
+    const PORT = ((): number => {
+      const strPort = env.PORT ?? argv[2];
+      return strPort !== undefined && !isNaN(+strPort) ? +strPort : 0;
+    })();
+
+    try {
+      const { port: openPort } = server
+        .listen(PORT, () => console.info(`Server: http://localhost:${openPort}`))
+        .address() as { port: number };
+    } catch (error) {
+      console.trace(error);
+      exit(1);
+    }
+  }
+
+  readonly #logger: Controller = ({ method, url }, res) => {
+    const start = Date.now();
+    res.on("finish", () => {
+      const time = Date.now() - start;
+      console.log(`[${method}] '\x1B[34m${url}\x1B[0m' - ${res.statusCode} - ${time}ms`);
+    });
+  };
+
+  readonly #shHandler: Controller = async (request, response) => {
+    const query = this.#extractQueryParams(request.url as string) as unknown as { command: string };
+    if (query.command === undefined) return response.writeHead(400).end();
+    if (typeof query.command !== "string") return response.writeHead(400).end();
+    try {
+      const { stderr, stdout } = await promisify(exec)(query.command as string);
+      if (stderr.length !== 0) throw new Error(stderr);
+      response.writeHead(200, { "Content-Type": "text/plain" }).write(stdout);
+      return response.end();
+    } catch (error) {
+      response.writeHead(400, { "Content-Type": "application/json" }).write(JSON.stringify(error));
+      return response.end();
+    }
+  };
+
+  readonly #fileHandler: Controller = async (request, response) => {
+    const query = this.#extractQueryParams(request.url as string) as unknown as { name: string };
+    if (query.name === undefined) return response.writeHead(400).end();
+    if (typeof query.name !== "string") return response.writeHead(400).end();
+    try {
+      const contentType = {
+        "Content-Type": this.#getContentType(extname(query.name)),
+      };
+      response.writeHead(200, contentType).write(await readFile(query.name));
+      return response.end();
+    } catch (error) {
+      console.trace(error);
+      response.writeHead(400).write(error);
+      return response.end();
+    }
+  };
+
+  readonly #getContentType = (type: string): string => {
+    if (type === ".html") return "text/html";
+    if (type === ".png") return "image/png";
+    if (type === ".jpg") return "image/jpg";
+    if (type === ".mp3") return "audio/mp3";
+    if (type === ".ogg") return "audio/ogg";
+    if (type === ".mp4") return "video/mp4";
+    if (type === ".mkv") return "video/mkv";
+    if (type === ".json") return "application/json";
+    return "";
+  };
+
+  readonly #extractQueryParams = (endpoint: string) => {
+    const [_, queryString] = endpoint.split("?");
+    if (queryString === undefined) return { query: {} };
+    const url = new URL("https://example.com" + endpoint);
+    const searchParams = new URLSearchParams(url.search);
+    return Object.fromEntries(searchParams.entries());
+  };
+};
+
+new ShellHttp();
